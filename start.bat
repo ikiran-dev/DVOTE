6e0c8730e2d05cb54ebf5e4945cec1d78d516232
ikiran-dev <ikiranrockrzz@gmail.com>
2023-04-06 10:37:37 +0530

    startup file changed

--- start.bat
+++ start.bat
@@ -1,63 +1,11 @@
 @echo off
 
 echo Running Truffle Migration...
-start cmd /c "truffle migrate --reset"
-if %errorlevel% neq 0 goto error1
-
-echo Waiting for 20 seconds...
-timeout /t 20 /nobreak
-
-echo Starting Dev Server...
-start cmd /c "npm run dev"
-if %errorlevel% neq 0 goto error2
-
-echo Done.
-pause
-exit
-
-:error1
-echo An error occurred while running Truffle Migration.
-pause
-exit /b %errorlevel%
-
-:error2
-echo An error occurred while starting the Dev Server.
-pause
-exit /b %errorlevel%
-@echo off
-
-echo Running Truffle Migration...
 start cmd /c "npx truffle migrate --reset"
 if %errorlevel% neq 0 goto error1
 
-echo Waiting for 20 seconds...
-timeout /t 20 /nobreak
-
-echo Starting Dev Server...
-start cmd /c "npm run dev"
-if %errorlevel% neq 0 goto error2
-
-echo Done.
-pause
-exit
-
-:error1
-echo An error occurred while running Truffle Migration.
-pause
-exit /b %errorlevel%
-
-:error2
-echo An error occurred while starting the Dev Server.
-pause
-exit /b %errorlevel%
-@echo off
-
-echo Running Truffle Migration...
-start cmd /c "npx truffle migrate --reset"
-if %errorlevel% neq 0 goto error1
-
-echo Waiting for 20 seconds...
-timeout /t 20 /nobreak
+echo Waiting for 40 seconds...
+timeout /t 40 /nobreak
 
 echo Starting Dev Server...
 start cmd /c "npm run dev"
